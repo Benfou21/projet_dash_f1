a3be4a456752278f905e151af2d2dba8fc0cda9d
benfou21 <benfou14210@gmail.com>
2024-02-14 16:34:24 +0100

    Time in seconds

--- src/dy_plot.py
+++ src/dy_plot.py
@@ -14,7 +14,10 @@ def get_circuit(data):
     x = data['x']
     y = data['y']
     speed = data['speed']
-    # x, y, speed = resample_based_on_time(data['x'], data['y'], data['speed'], data['speed'])
+    
+    # time_str = data["time"]   # c'est une chaîne de caractères qui ressemble à une liste
+    # time_list = ast.literal_eval(time_str)
+    # x, y, speed = resample_based_on_time(data['x'], data['y'], data['speed'], time_list)
     
     # Add the trace of the circuit
     # fig.add_trace(go.Scatter(x=x, y=y, mode='lines', name='Track'))
@@ -107,28 +110,29 @@ def get_bars(data):
     
 
     # Convertir la chaîne en liste
-    time_str = data["time"]  # c'est une chaîne de caractères qui ressemble à une liste
+    time_str = data["time"]   # c'est une chaîne de caractères qui ressemble à une liste
     time_list = ast.literal_eval(time_str)
-    print(time_list)
+    # print(time_list)
     
     tickvals = [i for i in range(len(time_list)) if i % 50 == 0]
 
     # Définir le texte des étiquettes pour correspondre à ces emplacements
     ticktext = [str(time_list[i]) for i in tickvals]
 
-    
+    # speed_normalized = (speed - min(speed)) / (max(speed) - min(speed))
+    # color_scale = [(v, f"rgb( {int(255 * (1 - v))},{int(255 * v)}, 0)") for v in speed_normalized]
     
     colors = [get_color(s) for s in speed]
     fig = go.Figure(data=[go.Bar(
         y=speed,
-        
         marker_color=colors  # Affecter les couleurs aux barres
     )])
 
     # Personnaliser le layout si nécessaire
     fig.update_layout(
         title='Vitesse sur différentes plages horaires', 
-        xaxis_title='Plage horaire', 
+        xaxis_title='Temps en seconde', 
+        yaxis_title='Vitesse en km/h', 
     )
     # Mettre à jour l'axe des x pour inclure les étiquettes de temps personnalisées
     fig.update_xaxes(tickvals=tickvals, ticktext=ticktext)
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -19,7 +19,7 @@ def load_and_save_telemetry() :
         'distance': []
     }
 
-    for year in range(2018, 2019):  # range of years
+    for year in range(2018, 2023):  # range of years
         # Load the qualifying session for Monza of the given year
         session = f1.get_session(year, 'Monza', 'Q')
         session.load()
@@ -37,7 +37,7 @@ def load_and_save_telemetry() :
         # Extract telemetry data
         telemetry = fastest_lap.get_telemetry()
         telemetry['Speed_m_s'] = telemetry['Speed'] / 3.6  # Convert speed to m/s
-        telemetry['Elapsed'] = telemetry['Time'].cumsum()
+        telemetry['Elapsed'] = telemetry['Time'].dt.total_seconds()
         # Calculate the distance for each telemetry point
         telemetry['Distance'] = telemetry['Speed_m_s'] * telemetry['Time'].diff().dt.total_seconds().fillna(0).cumsum()
         
@@ -47,7 +47,7 @@ def load_and_save_telemetry() :
         telemetry_data['y'].append(telemetry['Y'].tolist())
         telemetry_data['speed'].append(telemetry['Speed'].tolist())
         telemetry_data['distance'].append(telemetry['Distance'].tolist())
-        telemetry_data['time'].append(telemetry['Elapsed'].dt.total_seconds().tolist())  # Store elapsed time in seconds
+        telemetry_data['time'].append(telemetry['Elapsed'].tolist()) # Store elapsed time in seconds
     
     telemetry_df = pd.DataFrame(telemetry_data)
     telemetry_df.to_csv("assets/data/telemetry.csv")
@@ -56,6 +56,60 @@ def load_and_save_telemetry() :
 
 # print(load_and_save_telemetry())
 
+
+def load_and_save_telemetry_race(year) :
+    
+    # Initialize a dictionary to hold your data
+    telemetry_data = {
+        'year': [],
+        'x': [],
+        'y': [],
+        'speed': [],
+        'time': [],
+        'distance': []
+    }
+
+    # Load the qualifying session for Monza of the given year
+    session = f1.get_session(year, 'Monza', 'Q')
+    session.load()
+    
+    # Get the pole position driver
+    pole_position_driver = session.results.iloc[0]['DriverNumber']
+    
+    # Load the race session to get lap data
+    race_session = f1.get_session(year, 'Monza', 'R')
+    race_session.load(telemetry=True)
+    
+    # Get the fastest lap data for the pole position driver
+    fastest_lap = race_session.laps.pick_driver(pole_position_driver).pick_fastest()
+    
+    # Extract telemetry data
+    telemetry = fastest_lap.get_telemetry()
+    telemetry['Speed_m_s'] = telemetry['Speed'] / 3.6  # Convert speed to m/s
+    telemetry['Elapsed'] = telemetry['Time'].dt.total_seconds()
+    
+    # Calculate the distance for each telemetry point
+    telemetry['Distance'] = telemetry['Speed_m_s'] * telemetry['Time'].diff().dt.total_seconds().fillna(0).cumsum()
+    
+    # Store telemetry data
+    telemetry_data['year'].append(year)
+    telemetry_data['x'].append(telemetry['X'].tolist())
+    telemetry_data['y'].append(telemetry['Y'].tolist())
+    telemetry_data['speed'].append(telemetry['Speed'].tolist())
+    telemetry_data['distance'].append(telemetry['Distance'].tolist())
+    telemetry_data['time'].append(telemetry['Elapsed'].tolist())  # Store elapsed time in seconds
+
+    telemetry_df = pd.DataFrame(telemetry_data)
+    telemetry_df.to_csv(f"assets/data/telemetry_{year}.csv")
+    
+    return telemetry_df
+
+# load_and_save_telemetry_race(2019)
+
+# telemetry_df = pd.read_csv("./assets/data/telemetry_2019.csv")
+
+# data = telemetry_df
+# print(data["time"])
 
 def resample_based_on_speed(x, y, speed):
     # Pseudo-code pour illustrer le concept
