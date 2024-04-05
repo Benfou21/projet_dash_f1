644ac6ecdbe87af5c4c6bc8d81122935e94cfbf3
Martin Cuenot <80880026+mart1501@users.noreply.github.com>
2024-04-05 19:33:11 -0400

    Add files via upload

---
+++ src/hover_template/hover_template_1_classement.py
@@ -0,0 +1,17 @@
+def get_classement_hover_template(name):
+    if name == 'Hamilton':
+        hover_template = ('<b>%{customdata[0]}<br>' +
+                      f'<b>{name}: </b>' + '%{customdata[5]} - %{customdata[7]}pts<br>' +
+                      '<b>Championnat du monde<br>' +
+                      f'<b>{name}: </b>' + '%{customdata[11]}e - %{customdata[9]}pts<br>' +
+                      '<extra></extra>'
+        )
+    elif name == 'Verstappen':
+        hover_template = ('<b>%{customdata[0]} - %{customdata[1]}<br>' +
+                      f'<b>{name}: </b>' + '%{customdata[6]} - %{customdata[8]}pts<br>' +
+                      '<b>Championnat du monde<br>' +
+                      f'<b>{name}: </b>' + '%{customdata[12]}e - %{customdata[10]}pts<br>' +
+                      '<extra></extra>'
+        )
+    else: hover_template = ''
+    return hover_template
